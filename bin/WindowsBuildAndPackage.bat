ed7e1e2dee2859666731c9ee953b51a8c0452658
Jared Loewenthal <Jared.Loewenthal@proton.me>
2024-01-28 10:25:29 -0500

    Finished Package Script

--- bin/WindowsBuildAndPackage.bat
+++ bin/WindowsBuildAndPackage.bat
@@ -4,7 +4,7 @@ cls
 cargo.exe build -r
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 copy ..\target\release\swiftlet.exe .\Swiftlet.exe
-rem Adding Zipping Relevant Files Here in Future
+powershell Compress-Archive -Force -Path .\Swiftlet.exe, .\audio, .\security -DestinationPath .\Swiftlet.zip
 
 echo.
 exit 0
