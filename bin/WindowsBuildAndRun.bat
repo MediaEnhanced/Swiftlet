c965010c9ec84cc485e81ac67e123c81edea3e67
bubalooshi <bubalooshi@gmail.com>
2024-01-28 10:51:28 -0500

    Update windows scripts to reflect new arg parsing.

--- bin/WindowsBuildAndRun.bat
+++ bin/WindowsBuildAndRun.bat
@@ -11,13 +11,13 @@ IF [%1] EQU [] (
 cargo.exe build
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
-Start "Networking Audio Server" "..\target\debug\swiftlet.exe" --name Realm
+Start "Networking Audio Server" "..\target\debug\swiftlet.exe" --sname Realm --ipv6
 timeout /t 1
-Start "Networking Audio Client1" "..\target\debug\swiftlet.exe" -n Jared ::1
+Start "Networking Audio Client1" "..\target\debug\swiftlet.exe" -u Jared -a [::1]:9001
 timeout /t 1
-Start "Networking Audio Client2" "..\target\debug\swiftlet.exe" -n Client2 ::1
+Start "Networking Audio Client2" "..\target\debug\swiftlet.exe" -u Client2 -a [::1]:9001
 timeout /t 1
-Start "Networking Audio Client3" "..\target\debug\swiftlet.exe" -n Client3 ::1
+Start "Networking Audio Client3" "..\target\debug\swiftlet.exe" -u Client3 -a [::1]:9001
 
 echo.
 exit 0
@@ -31,13 +31,13 @@ IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 rem stripping is now part of the cargo build process
 rem strip.exe -o ..\target\release\swiftlet.exe ..\target\release\swiftlet.exe
 
-Start "Networking Audio Server" "..\target\release\swiftlet.exe" --name Realm
+Start "Networking Audio Server" "..\target\release\swiftlet.exe" --sname Realm --ipv6
 timeout /t 1
-Start "Networking Audio Client1" "..\target\release\swiftlet.exe" -n Jared ::1
+Start "Networking Audio Client1" "..\target\release\swiftlet.exe" -u Jared -a [::1]:9001
 timeout /t 1
-Start "Networking Audio Client2" "..\target\release\swiftlet.exe" -n Client2 ::1
+Start "Networking Audio Client2" "..\target\release\swiftlet.exe" -u Client2 -a [::1]:9001
 timeout /t 1
-Start "Networking Audio Client3" "..\target\release\swiftlet.exe" -n Client3 ::1
+Start "Networking Audio Client3" "..\target\release\swiftlet.exe" -u Client3 -a [::1]:9001
 
 echo.
 exit 0
